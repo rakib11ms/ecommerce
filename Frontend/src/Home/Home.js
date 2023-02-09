ff42135b349dac4999d50a496e0e69975364b776
Md. Rakib Hossain <rakib.ba@pakiza.com>
2023-02-09 17:26:50 +0600

    updyaed

--- Frontend/src/Home/Home.js
+++ Frontend/src/Home/Home.js
@@ -157,7 +157,7 @@ function Home() {
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 4
+            items: 3
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
